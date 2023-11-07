ca584fee2ed5876e1debbd93a0b734e81ffc5fce
agustinnavas <145386817+agustinnavas@users.noreply.github.com>
2023-11-07 20:54:00 +0100

    primer commit examen

---
+++ src/main/java/com/example/examenesgynny/ExamenesGynnyApplication.java
@@ -0,0 +1,13 @@
+package com.example.examenesgynny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenesGynnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenesGynnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examenesgynny/ExamenesGynnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenesgynny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenesGynnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
